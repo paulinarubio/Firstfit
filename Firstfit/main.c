ad8317f6f11f1c22c63d7cd53acecef8075b83be
paulinarubio <Paulina@Paulinas-MacBook-Pro.local>
2013-12-02 14:55:04 -0800

    upl.

--- Firstfit/main.c
+++ Firstfit/main.c
@@ -4,15 +4,47 @@
 //
 //  Created by Paulina Rubio Tarriba on 10/20/13.
 //  Copyright (c) 2013 Paulina Rubio Tarriba. All rights reserved.
-//
-
+/
 #include <stdio.h>
+#include <conio.h>
 
-int main(int argc, const char * argv[])
-{
 
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+
+    int main(int argc, const char * argv[])
+    {
+        
+        int p,m;
+        printf("Enter number of processes:");
+        scanf("%d",&p);
+        printf("Enter number of Memory blocks:");
+        scanf("%d",&m);
+        
+        int parr[p],marr[m],i;
+        for(i=0;i<p;i++)
+        {
+            printf("Enter size of process %d:",i+1);
+            scanf("%d",&parr[i]);
+        }
+        for(i=0;i<m;i++)
+        {
+            printf("Enter size of memory %d:",i+1);
+            scanf("%d",&marr[i]);
+        }
+        int j;
+        for(i=0;i<p;i++){
+            for(j=0;j<m;j++){
+                if(marr[j]>=parr[i]){
+                    marr[j]-=parr[i];
+                    printf("Allocating process %d to memory %d\n Size remaining in it after allocation %d\n\n",i+1,j+1,marr[j]);
+                    break;
+                }
+                
+                
+            }
+            if(j==m)
+            {printf("Not enough memory for process %d",i);break;}
+        }
+        getch();
+
 }
 
